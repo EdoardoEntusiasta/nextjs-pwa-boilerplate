de9f41d38a65cf28455e8063ce3215e7af89e73a
Edoardo <edoardo.casella@entusiasta.tech>
2022-02-04 13:47:08 +0400

    Hook import

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -2,13 +2,15 @@ import { useDevice } from './useDevice';
 import { useViewport } from './useViewport';
 import { useWebPSupport } from './useWebPSupport';
 import { useScroll } from './useScroll';
+import { useLocalStorage } from './useLocalStorage';
 
 
 export {
     useDevice,
     useViewport,
     useWebPSupport,
-    useScroll
+    useScroll,
+    useLocalStorage
 };
 
 
