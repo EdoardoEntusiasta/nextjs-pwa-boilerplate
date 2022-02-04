08117c58a044690361cfd41277e18a9dc5dfe8c4
Edoardo <edoardo.casella@entusiasta.tech>
2022-02-04 13:14:16 +0400

    useScroll

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,12 +1,14 @@
 import { useDevice } from './useDevice';
 import { useViewport } from './useViewport';
 import { useWebPSupport } from './useWebPSupport';
+import { useScroll } from './useScroll';
 
 
 export {
     useDevice,
     useViewport,
-    useWebPSupport
+    useWebPSupport,
+    useScroll
 };
 
 
