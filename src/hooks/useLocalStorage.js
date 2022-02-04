4a3fba413c4817097e459988e0cc39ff2de77f2a
Edoardo <edoardo.casella@entusiasta.tech>
2022-02-04 13:46:12 +0400

    Comment

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -1,3 +1,14 @@
+/**
+ * Sync state to local storage so that it persists through a page refresh. 
+ * Usage is similar to useState except we pass in a local storage key so that we can default 
+ * to that value on page load instead of the specified initial value.
+ * @param {*} key 
+ * @param {*} initialValue 
+ * @returns 
+ */
+
+
+
 // Hook
 export const useLocalStorage = (key, initialValue) => {
   // State to store our value
